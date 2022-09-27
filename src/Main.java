a7498ea0873a944205a7adeac45ccc9002f3aec1
Dels1x <delsixreborn@gmail.com>
2022-09-27 13:00:48 +0300

    my first postgresql CLI program

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,65 @@
+import java.sql.*;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
 
-        Example example = new Example();
-        Example example1 = new Example();
+    private static final String DB_USERNAME = "postgres";
+    private static final String DB_PASSWORD = "password";
+    private static final String DB_URL = "jdbc:postgresql://localhost:5432/postgres";
 
-        System.out.println(example.integer);
-        System.out.println(example.character);
-        System.out.println(example.string);
+    public static void main(String[] args) throws SQLException {
 
-        example.incrementBy(5);
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println(example1.integer);
+        Connection connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
 
-        example1.incrementBy(6);
+        while (true) {
+            System.out.println("1. Show all tasks");
+            System.out.println("2. Complete task");
+            System.out.println("3. Create task");
+            System.out.println("4. Quit");
 
-        System.out.println(Example.integer);
+            int command = scanner.nextInt();
+
+            if (command == 1) {
+                Statement statement = connection.createStatement();
+                ResultSet result = statement.executeQuery("SELECT * FROM task ORDER BY id DESC");
+
+                System.out.println("Id\tName\tState");
+
+                while (result.next()) {
+                    System.out.println(result.getInt("id") + "\t" +
+                            result.getString("name") + "\t" +
+                            result.getString("state"));
+                }
+            } else if (command == 2) {
+                String sql = "UPDATE task SET state = 'DONE' where id = ?";
+                PreparedStatement preparedStatement = connection.prepareStatement(sql);
+                System.out.println("Enter ID of task: ");
+                int id = scanner.nextInt();
+                preparedStatement.setInt(1, id);
+                preparedStatement.execute();
+            } else if (command == 3) {
+                String sql = "INSERT INTO task (name, state) VALUES (?, ?)";
+                PreparedStatement preparedStatement = connection.prepareStatement(sql);
+                System.out.println("Enter name of task");
+                scanner.nextLine();
+                String name = scanner.nextLine();
+                System.out.println("Enter state of task");
+                String state = scanner.nextLine();
+
+                if(name.length() > 54)
+                    name = name.substring(0, 54);
+                if(state.length() > 54)
+                    state = state.substring(0, 54);
+                preparedStatement.setString(1, name);
+                preparedStatement.setString(2, state);
+                preparedStatement.execute();
+            } else if (command == 4) {
+                System.exit(0);
+            } else {
+                System.err.println("Unknown command");
+            }
+        }
     }
 }
